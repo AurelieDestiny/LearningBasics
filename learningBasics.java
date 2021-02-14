e6235c5e379b00310aeb2dd0041264360951c166
AurelieDestiny <78560576+AurelieDestiny@users.noreply.github.com>
2021-02-13 22:58:11 -0500

    Adding new files

---
+++ learningBasics.java
@@ -0,0 +1,19 @@
+public class learningBasics {
+	
+public static void main(String[] args) {
+		
+int m=12,n=5; 
+int s1 = m+n; // 17
+int s2 = m-n; // 7
+int s3 = m*n; // 60
+double s4 = (double)m/n; // 3
+int s5 = m%n;  // 
+
+System.out.println(s1);
+System.out.println(s2);
+System.out.println(s3);
+System.out.println(s4);
+System.out.println(s5);
+	}
+
+}
